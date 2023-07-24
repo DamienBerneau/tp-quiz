ac76ee6e69f28dd5584cf948c665bf9d4cf0c8ed
Noemiiie <noemierouc@gmail.com>
2023-07-24 11:48:28 +0200

    Menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-// import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from './components/Header'
+import './App.css'
+import Menu from './components/Menu'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Header></Header>
+      <Menu />
     </>
   )
 }
