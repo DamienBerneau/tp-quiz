3da06998ea58137f28154c7dd5a5c5d2d18f6057
Noemiiie <noemierouc@gmail.com>
2023-07-24 15:03:56 +0200

    Conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
 
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from './components/Header'
 import User from './components/user'
 import Quizz from "./components/Quizz";
-import './components/Quizz.jsx'
-import './App.css'
-
-// import 'bootstrap/dist/css/bootstrap.min.css'
-
-import { useState } from 'react';
-import './App.css'
 import Main from './components/Main.jsx'
 import QuizPage from './components/QuizPage.jsx'
-import Header from './components/Header.jsx'
-import User from './components/user.jsx'
+import './App.css'
 
 function App() {
   const [user, setUser] = useState('')
@@ -22,7 +15,7 @@ function App() {
       <div className='min-vh-100 min-vw-100'>
         <Header user={user} />
         <User setUser={setUser} />
-        <Header/>
+        <Header />
         <Main />
         <QuizPage />
         <Quizz />
