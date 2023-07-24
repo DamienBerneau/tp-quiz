35628035d2cb217bd0a873a471a16c50b1fba5ff
HiX <133093398+HiixX@users.noreply.github.com>
2023-07-24 11:55:32 +0200

    maj

--- src/components/Quizz.jsx
+++ src/components/Quizz.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 
 const Quizz = () => {
   const [questions, setQuestions] = useState([]);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
 
   useEffect(() => {
     const fetchQuestions = async () => {
@@ -10,6 +12,7 @@ const Quizz = () => {
         const data = await response.json();
         if (data.response_code === 0) {
           setQuestions(data.results);
+          setUserAnswers(new Array(data.results.length).fill(null));
         } else {
           console.error('La requête n\'a pas abouti. Veuillez réessayer plus tard.');
         }
@@ -22,28 +25,37 @@ const Quizz = () => {
   }, []);
 
   const handleAnswerSelect = (questionIndex, selectedAnswer) => {
-    // Vous pouvez implémenter ici la logique pour traiter la réponse sélectionnée
-    console.log(`Question ${questionIndex + 1}, réponse sélectionnée: ${selectedAnswer}`);
+    setUserAnswers((prevAnswers) => {
+      const updatedAnswers = [...prevAnswers];
+      updatedAnswers[questionIndex] = selectedAnswer;
+      return updatedAnswers;
+    });
+
+    if (questionIndex < questions.length - 1) {
+      setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
+    }
   };
 
+  if (questions.length === 0) {
+    return <div>Chargement...</div>;
+  }
 
-  
+  const currentQuestion = questions[currentQuestionIndex];
+  const allAnswers = currentQuestion.incorrect_answers.concat(currentQuestion.correct_answer).sort(() => Math.random() - 0.5);
+
   return (
     <div>
-      {questions.map((question, index) => (
-        <div key={index}>
-          <p>{index + 1}. {question.question}</p>
-          {question.incorrect_answers.concat(question.correct_answer).sort(() => Math.random() - 0.5).map((answer, answerIndex) => (
-            <div key={answerIndex}>
-              <input
-                type="radio"
-                name={`q${index}`}
-                value={answer}
-                onChange={() => handleAnswerSelect(index, answer)}
-              />
-              <label htmlFor={`q${index}`}>{answer}</label>
-            </div>
-          ))}
+      <p>{currentQuestionIndex + 1}. {currentQuestion.question}</p>
+      {allAnswers.map((answer, answerIndex) => (
+        <div key={answerIndex}>
+          <input
+            type="radio"
+            name={`q${currentQuestionIndex}`}
+            value={answer}
+            checked={userAnswers[currentQuestionIndex] === answer}
+            onChange={() => handleAnswerSelect(currentQuestionIndex, answer)}
+          />
+          <label htmlFor={`q${currentQuestionIndex}`}>{answer}</label>
         </div>
       ))}
     </div>
